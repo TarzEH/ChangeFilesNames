a6d853ef00ac83dccb6e9899be827894e5ea02d7
Ori Ashkenazi <70447976+OriZzZ-94@users.noreply.github.com>
2022-09-17 18:52:26 +0300

    Configure Reem Gui with the code

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
 import os
 from tkinter import *
-#GUI
-
-from tkinter import *
-root = Tk()
-
-root.title("Change files name app")
-root.configure(width=500, height=300)
-root.configure(bg='lightgray')
-label1 = Label(root, padx=50, pady=100, text="Hi, insert Directory Name: ")
-label1.pack()
-import_user = Entry(root)
-import_user.pack()
-MyButton = Button(root, pady=50, padx=50, text="submit")
-MyButton.pack()
-
-
-root.mainloop()
+from functools import partial
 
 
 #Functions
@@ -47,18 +31,48 @@ def condition_function(filename):
 
 
 #Program
-os.chdir(os.path.join(os.getenv('HOME'), 'Desktop'))
-directory = input("insert Directory Name:")
-path=os.walk(os.getcwd())
-for root, directories, files in path:
-        for path in os.listdir(root):
-            full_path = os.path.join(root, path)
-            #if os.path.isfile(full_path):
-            if directory in full_path and not os.path.isfile(full_path):
-                for filename in os.listdir(full_path):
-                    check_file = os.path.join(full_path, filename)
-                    if os.path.isfile(check_file):
-                        condition_function(check_file)
+def run(directory,env):
+    os.chdir(os.path.join(os.getenv(directory), env))
+    directory = input("insert Directory Name:")
+    path=os.walk(os.getcwd())
+    for root, directories, files in path:
+            for path in os.listdir(root):
+                full_path = os.path.join(root, path)
+                #if os.path.isfile(full_path):
+                if directory in full_path and not os.path.isfile(full_path):
+                    for filename in os.listdir(full_path):
+                        check_file = os.path.join(full_path, filename)
+                        if os.path.isfile(check_file):
+                            condition_function(check_file)
+
+
+
+
+
+
+if __name__ == "__main__":
+    #GUI
+    root = Tk()
+
+    root.title("Change files name app")
+    root.configure(width=500, height=300)
+    root.configure(bg='lightgray')
+    label1 = Label(root, padx=50, pady=20, text="Hi, insert Directory Name: ")
+    label1.pack()
+    import_user = Entry(root)
+    import_user.pack()
+    label2 = Label(root, padx=70, pady=20, text="insert Env Name: ")
+    label2.pack()
+    import_user2 = Entry(root)
+    import_user2.pack()
+    MyButton = Button(root, pady=10, padx=50, text="submit", command=lambda: run(import_user.get(),import_user2.get()))
+    MyButton.pack()
+
+
+    root.mainloop()
+
+
+
 
 
 
