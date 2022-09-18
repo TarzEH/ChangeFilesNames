f972cff54f57594a28cd6c6aeee3ae3ccabf37a3
reemnu <reemnu@gmail.com>
2022-09-18 16:37:06 +0300

    GUI update

---
+++ gui_file.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
+
+# create the root window
+root = tk.Tk()
+root.title('Change files name app')
+root.resizable(False, False)
+root.geometry('300x150')
+
+
+def select_files():
+    filetypes = (
+        ('text files', '*.txt'),
+        ('All files', '*.*')
+    )
+
+    filenames = fd.askopenfilenames(
+        title='Open files',
+        initialdir='/',
+        filetypes=filetypes)
+
+    showinfo(
+        title='Selected Files',
+        message=filenames
+    )
+
+
+# open button
+open_button = ttk.Button(
+    root,
+    text='Open Files',
+    command=select_files
+)
+
+open_button.pack(expand=True)
+
+root.mainloop()
